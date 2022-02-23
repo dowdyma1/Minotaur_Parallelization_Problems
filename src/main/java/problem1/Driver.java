ec285e2f92579b73137511b232208e194d57ba01
dowdyma1 <dowdyma1@gmail.com>
2022-02-23 15:58:39 -0500

    initial commit

---
+++ src/main/java/problem1/Driver.java
@@ -0,0 +1,7 @@
+package src.main.java.problem1;
+
+public class Driver {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/problem1/Guest.java
@@ -0,0 +1,8 @@
+package src.main.java.problem1;
+
+public class Guest implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
